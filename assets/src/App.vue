9d6a6ce9aa4f9fe2b18dab576ce197f75238bc85
Florian Moser <products@famoser.ch>
2019-08-12 15:00:34 +0200

    Add vuetify dependency

--- assets/src/App.vue
+++ assets/src/App.vue
@@ -1,43 +1,37 @@
 <template>
-    <div id="app">
-        <CreateRelease @create-release="openReleaseDialog"></CreateRelease>
-        <p>{{currentlyOpen}}</p>
-        <img alt="Vue logo" src="./assets/logo.png"/>
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from "vue-property-decorator";
-    import HelloWorld from "./components/HelloWorld.vue";
-    import CreateRelease from "@/components/CreateRelease.vue";
+import Vue from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 
-    @Component({
-        components: {
-            CreateRelease,
-            HelloWorld
-        }
-    })
-    export default class App extends Vue {
-        isOpen: Boolean = false;
-
-        openReleaseDialog() {
-            this.isOpen = true;
-        }
-
-        get currentlyOpen() {
-            return this.isOpen ? "yes" : "no";
-        }
-    }
+export default Vue.extend({
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+});
 </script>
-
-<style lang="scss">
-    #app {
-        font-family: "Avenir", Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-</style>
